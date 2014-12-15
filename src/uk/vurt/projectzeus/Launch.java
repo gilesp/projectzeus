ee6159dee03a7666ae97038f24ee763cc0984a37
Giles Paterson <giles.paterson@blackpepper.co.uk>
2014-12-15 16:35:12 +0000

    Added stub project layout & code

---
+++ src/uk/vurt/projectzeus/Launch.java
@@ -0,0 +1,15 @@
+package uk.vurt.projectzeus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Launch extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
